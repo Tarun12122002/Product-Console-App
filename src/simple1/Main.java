50c7a94d54c3f924acd1a04625da594b9d591a32
Tarun12122002 <tarun12122002@gmail.com>
2025-10-04 09:57:52 +0530

    Update Main.java

--- src/simple1/Main.java
+++ src/simple1/Main.java
@@ -6,14 +6,19 @@ public class Main {
         while (true) {
             System.out.println("Welcome to E-Commerce Console");
             System.out.println("---------------------------------");
-            System.out.println("Choose User:");
-            System.out.print("Enter choice: ");
+            System.out.println(" Hi user you are curently in home page!");
+            System.out.println(" You can choose any 1 from the below ");
+            System.out.print(" Enter 1 if you are an admin\r\n"
+            		+ " Enter 2 if you want to purchase any products from our E-Commerce\r\n"
+            		+ " Enter 3 to exit this app\r\n"
+            		);
+            System.out.print(" Enter your choice: ");
             int user = sc.nextInt();
             switch (user) {
                 case 1: admin.adminMenu(); break;
                 case 2: customer.customerMenu(); break;
-                case 3: System.out.println("Exiting..."); return;
-                default: System.out.println("Invalid Choice!");
+                case 3: System.out.println(" Exiting..."); return;
+                default: System.out.println(" Invalid Choice!");
             }
         }
     }
